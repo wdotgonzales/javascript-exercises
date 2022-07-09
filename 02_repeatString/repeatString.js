fc9ce0154ba393b4e001b043f06c173e4243a294
Wilson Gonzales <wdotgonzales@gmail.com>
2022-07-09 14:08:41 +0800

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(str,num) {
+let arr = [];
 
+  for(let x = 0; x < num;x++){
+    arr.push(str);
+  }
+
+  const word = arr.join("")
+  return word;
 };
 
 // Do not edit below this line
